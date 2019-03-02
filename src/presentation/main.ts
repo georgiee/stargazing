8963ad20bbca10fa5e64ba574ca555efa1673e50
Georgios Kaleadis <georgios@kaleadis.de>
2019-03-02 14:20:44 +0100

    dasharray explained with animation and virtual fragments

--- src/presentation/main.ts
+++ src/presentation/main.ts
@@ -1,1 +1,56 @@
-console.log('hello main');
+// import { SlideBuilder } from './slide-builder';
+
+// function run() {
+//   const dashAnimationBuilder = new SlideBuilder();
+//   dashAnimationBuilder.fragments([
+//     () => {
+//       console.log('fragment 1');
+//       nextFrame();
+//     },
+//     () => {
+//       console.log('fragment 2');
+//       nextFrame();
+//     },
+//     () => {
+//       console.log('fragment 3');
+//       nextFrame();
+//     },
+//   ], () => {
+//     console.log('hidden');
+//     previousFrame();
+//   });
+// }
+// window.addEventListener('DOMContentLoaded', run);
+
+// const dashDemoFragments = new SlideBuilder(dashdemo);
+//       dashDemoFragments.fragments([
+//         () => {
+//           console.log('fragment 1');
+//           nextFrame();
+//         },
+//         () => {
+//           console.log('fragment 2');
+//           nextFrame();
+//         },
+//         () => {
+//           console.log('fragment 3');
+//           nextFrame();
+//         },
+//       ], () => {
+//         console.log('hidden');
+//         previousFrame();
+//       });
+
+//       function nextFrame() {
+//         const iframe = dashdemo;
+//         const content = iframe.contentWindow;;
+//         console.log('nextFrame', content)
+
+//         content.postMessage("next-topic", "*");
+//       }
+
+//       function previousFrame() {
+//         const iframe = dashdemo;
+//         const content = iframe.contentWindow;;
+//         content.postMessage("previous-topic", "*");
+//       }
---
+++ src/presentation/typings.d.ts
@@ -0,0 +1,4 @@
+declare function require(name: string): string;
+/** The number of widgets present */
+declare const Reveal: any;
+declare const __CPEmbed;
