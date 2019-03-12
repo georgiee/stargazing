12fc6fd7a902e608168512fc703d8e481068111b
Georgios Kaleadis <georgios@kaleadis.de>
2019-03-12 07:34:51 +0100

    slide code snippet updates

--- examples/constellations/constellation-02a/snippet-path.js
+++ examples/constellations/constellation-02a/snippet-path.js
@@ -1,20 +1,9 @@
-const points = [ {x: 0, y: 0}, {x: 100, y: 0}];
-
-createDots(points); //easy, we did this plenty of times
-createLine(points);
-
-function createLine(line) {
+function createLine(linepoints) {
   const path = createSVGElement('path');
-  path.setAttribute('d', generateLinePath(...line));
-  svg.appendChild(path);
-}
 
-// generates this string: `M 0 0 L 100 0`
-function generateLinePath(...points) {
-  const [startPoint, ...segmentPoints] = points;
-  const lineSegments = segmentPoints.map(({x, y}) => `L ${x} ${y}`);
-  const pathParts = [
-    `M ${startPoint.x} ${startPoint.y}`, ...lineSegments
-  ];
-  return pathParts.join(' ');
+  // generates this string: `M 0 0 L 100 0`
+  const pathData = generateLinePath(...linepoints);
+
+  path.setAttribute('d', pathData);
+  svg.appendChild(path);
 }
